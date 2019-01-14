3ca293b667a2cd458e3605d0213c4f03a048ef99
soyccan <soyccan@soyccan-vm-server>
2019-01-14 05:33:32 +0000

    custom admin site

--- calculuseal/calculuseal/settings.py
+++ calculuseal/calculuseal/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'calculuseal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
