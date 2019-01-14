67eaeec8ad795ce1f7662e66175a4c2fc289682e
soyccan <soyccan@soyccan-vm-server>
2019-01-14 02:50:23 +0000

    admin site

--- calculuseal/webhook/admin.py
+++ calculuseal/webhook/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from webhook import models
 
-admin.site.register(models.Media)
-admin.site.register(models.Friends)
+# admin.site.register(models.Media)
+# admin.site.register(models.Friends)
+
+@admin.register(models.Media)
+class MediaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Friends)
+class FriendsAdmin(admin.ModelAdmin):
+    pass
