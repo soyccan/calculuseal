2cd02b89672576a8b7e58db53ef6455fe21de709
soyccan <soyccan@soyccan-vm-server>
2019-01-25 08:21:41 +0000

    add config

---
+++ calculuseal/config/line.py
@@ -0,0 +1,9 @@
+# define data related to LINE channel
+
+# line user ID (the API one, not usual one) of me, the developer
+DEVELOPER_ID = 'U7c2232b80dc0104731e13349fb24c6d7'
+
+# channel tokens
+CHANNEL_ACCESS_TOKEN = '4V2vJYsE2KxV4iBibUWmrTnFLa4fSGaqJtFGMB90ozv1jLUGtUsSPU66fmrMH7EOnXx8MUtGjrpGN8ueV2dEbSrsi/nyHgE5aQVw79jnKI/fLfkYcHms5bsphNLnTV/ItDfXdQOcnVmGRRUxxF6XnAdB04t89/1O/w1cDnyilFU='
+CHANNEL_SECRET = '6339ec663db70f8103a5e0471c0bc2b3'
+
