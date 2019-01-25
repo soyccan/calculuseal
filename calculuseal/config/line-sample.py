c937364309aecac5f50d077b811790d5df7a9f3f
soyccan <soyccan@soyccan-vm-server>
2019-01-25 07:51:42 +0000

    remove secret and add sample config file

---
+++ calculuseal/config/line-sample.py
@@ -0,0 +1,9 @@
+# define data related to LINE channel
+
+# line user ID (the API one, not usual one) of me, the developer
+DEVELOPER_ID = 'U1234567890abcdef1234567890abcdef'
+
+# channel tokens
+CHANNEL_ACCESS_TOKEN = '<base64 encoded token>'
+CHANNEL_SECRET = '1234567890abcdef1234567890abcdef'
+
